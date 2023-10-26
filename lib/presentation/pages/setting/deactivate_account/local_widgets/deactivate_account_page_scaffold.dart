5989fc365dc1f7934eaafbfb5df8e761db69ad04
Shahil <shahil1.jha@gmail.com>
2023-10-26 23:57:15 +0930

    deactivate

--- lib/presentation/pages/setting/deactivate_account/local_widgets/deactivate_account_page_scaffold.dart
+++ lib/presentation/pages/setting/deactivate_account/local_widgets/deactivate_account_page_scaffold.dart
@@ -1,7 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:config/config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../application/auth/auth_bloc.dart';
+import '../../../../routes/router.dart';
 import '../../../../widgets/app_button.dart';
 
 class DeactivateAccountPageScaffold extends StatelessWidget {
@@ -9,59 +12,69 @@ class DeactivateAccountPageScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 100),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  InkWell(
-                    onTap: () => context.router.pop(),
-                    child: Image.asset('assets/icons/back_button.png'),
-                  ),
-                  const SizedBox(width: 15),
-                  Text(
-                    'Settings',
-                    style: Theme.of(context).textTheme.displayMedium,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 45),
-              Text(
-                'Deactivate Account',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              const SizedBox(height: 8),
-              Text(
-                'Are you sure you want to deactivate your account?\nPlease keep in mind that deactivating an account is irreversible.',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              const SizedBox(height: 18),
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(vertical: 22),
-                // height: 300,
-                decoration: const BoxDecoration(
-                  color: kContainerGreyBackground,
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        state.maybeMap(
+          unauthenticated: (_) => context.router.replaceNamed(rLogin),
+          orElse: () => null,
+        );
+      },
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 100),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    InkWell(
+                      onTap: () => context.router.pop(),
+                      child: Image.asset('assets/icons/back_button.png'),
+                    ),
+                    const SizedBox(width: 15),
+                    Text(
+                      'Settings',
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                  ],
                 ),
-                child: const Column(
-                  children: [],
+                const SizedBox(height: 45),
+                Text(
+                  'Deactivate Account',
+                  style: Theme.of(context).textTheme.bodyLarge,
                 ),
-              ),
-              const SizedBox(height: 20),
-              AppButton(
-                textString: 'Deactivate',
-                margin: const EdgeInsets.all(0),
-                color: kTileDeleteButtonColor,
-                onPressed: () async {},
-              ),
-            ],
+                const SizedBox(height: 8),
+                Text(
+                  'Are you sure you want to deactivate your account?\nPlease keep in mind that deactivating an account is irreversible.',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                const SizedBox(height: 18),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 22),
+                  // height: 300,
+                  decoration: const BoxDecoration(
+                    color: kContainerGreyBackground,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: const Column(
+                    children: [],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                AppButton(
+                  textString: 'Deactivate',
+                  margin: const EdgeInsets.all(0),
+                  color: kTileDeleteButtonColor,
+                  onPressed: () => context.read<AuthBloc>().add(
+                        const AuthEvent.deactivateAccount(),
+                      ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
