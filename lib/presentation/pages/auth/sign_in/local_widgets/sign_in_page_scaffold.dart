6b8d30b908f0c6db825f61795ca287ff0e27a916
Shahil <shahil1.jha@gmail.com>
2023-10-26 17:10:46 +0930

    style: refactor

--- lib/presentation/pages/auth/sign_in/local_widgets/sign_in_page_scaffold.dart
+++ lib/presentation/pages/auth/sign_in/local_widgets/sign_in_page_scaffold.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:dartz/dartz.dart';
-import 'package:enjoy_nt/presentation/routes/router.dart';
 import 'package:enjoy_nt/presentation/routes/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -48,11 +47,8 @@ class SignInPageScaffold extends StatelessWidget {
               context
                   .read<SignInFormBloc>()
                   .add(const SignInFormEvent.rememberEmail());
-              //TODO: navigation
               //Navigation
-              // context.router.replaceNamed(rHome);
-              // context.router.popUntil((route) => route.settings.name == rLogin);
-              context.router.replaceAll([HomeRoute()]);
+              context.router.replaceAll([const HomeRoute()]);
               //making the auth state in the AuthBloc as _authenticated_
               context
                   .read<AuthBloc>()
