779ba5c06ae0de16da8249b34cb3c30ee7dbd60b
Shahil <shahil1.jha@gmail.com>
2023-10-27 01:44:15 +0930

    search wip

--- lib/infrastructure/core/injectable_modules/package_injectable_module.dart
+++ lib/infrastructure/core/injectable_modules/package_injectable_module.dart
@@ -1,9 +1,17 @@
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:algolia/algolia.dart';
 
 @module
 abstract class PackageInjectableModule {
   @lazySingleton
   @preResolve
   Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
+
+  @lazySingleton
+  @preResolve
+  Algolia get algolia => const Algolia.init(
+        applicationId: 'JD88YGON0U',
+        apiKey: '015d67e7b78d3db34fb2466216aaa3bc',
+      ).instance;
 }
--- lib/presentation/widgets/app_search_bar.dart
+++ lib/presentation/widgets/app_search_bar.dart
@@ -1,4 +1,6 @@
+import 'package:algolia/algolia.dart';
 import 'package:config/config.dart';
+import 'package:enjoy_nt/injection.dart';
 import 'package:flutter/material.dart';
 
 class AppSearchBar extends StatefulWidget {
@@ -10,61 +12,84 @@ class AppSearchBar extends StatefulWidget {
 }
 
 class _AppSearchBarState extends State<AppSearchBar> {
+  List _queryResults = [];
+
+  Future<void> _getQueryResults(String queryStr) async {
+    print('QUERY STR =========> $queryStr');
+    AlgoliaQuerySnapshot snap = await getIt<Algolia>()
+        .index('ENJOY_NT_SEARCH')
+        .query(queryStr)
+        .getObjects();
+    snap.hits.asMap().entries.forEach(
+          (element) => print(element),
+        );
+    print(snap.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 45,
       child: SearchAnchor(
+          isFullScreen: false,
           builder: (BuildContext context, SearchController controller) {
-        return SearchBar(
-          controller: controller,
-          padding: const MaterialStatePropertyAll<EdgeInsets>(
-            EdgeInsets.symmetric(horizontal: 16.0),
-          ),
-          hintText: 'Search',
-          hintStyle: const MaterialStatePropertyAll<TextStyle>(
-            TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          elevation: const MaterialStatePropertyAll<double>(0),
-          shape: const MaterialStatePropertyAll<OutlinedBorder>(
-            RoundedRectangleBorder(
-              side: BorderSide(
-                color: kSearchBarborder,
-                width: 1.5,
+            return SearchBar(
+              controller: controller,
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
+                EdgeInsets.symmetric(horizontal: 16.0),
               ),
-              borderRadius: BorderRadius.all(
-                Radius.circular(8),
+              hintText: 'Search',
+              hintStyle: const MaterialStatePropertyAll<TextStyle>(
+                TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-          ),
-          onTap: () {
-            controller.openView();
+              elevation: const MaterialStatePropertyAll<double>(0),
+              shape: const MaterialStatePropertyAll<OutlinedBorder>(
+                RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: kSearchBarborder,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                ),
+              ),
+              onTap: () {
+                controller.openView();
+                print('PRESSED onTap ==============> ${controller.text}');
+              },
+              onChanged: (value) {
+                print(
+                    'PRESSED onCHnaged ==============> ${controller.value.text}');
+                print('QUERY VALUE STR =========> $value');
+
+                _getQueryResults(value);
+              },
+              onSubmitted: (value) => _getQueryResults,
+              leading: const Icon(
+                Icons.search,
+                color: kPrimaryColor,
+              ),
+            );
           },
-          onChanged: (_) {
-            controller.openView();
-          },
-          leading: const Icon(
-            Icons.search,
-            color: kPrimaryColor,
-          ),
-        );
-      }, suggestionsBuilder:
+          suggestionsBuilder:
               (BuildContext context, SearchController controller) {
-        return List<ListTile>.generate(5, (int index) {
-          final String item = 'item $index';
-          return ListTile(
-            title: Text(item),
-            onTap: () {
-              setState(() {
-                controller.closeView(item);
-              });
-            },
-          );
-        });
-      }),
+            return List<ListTile>.generate(5, (int index) {
+              final String item = 'item $index';
+              return ListTile(
+                title: Text(item),
+                onTap: () {
+                  print('============> ${controller.text}');
+                  setState(() {
+                    controller.closeView(item);
+                  });
+                },
+              );
+            });
+          }),
     );
   }
 }
